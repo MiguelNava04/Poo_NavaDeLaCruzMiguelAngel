78f2bfafd03e6547f5c2bd23192a7f26c0b6589f
MiguelNava04 <ma.nava.dlc@edu.utc.mx>
2021-12-06 22:46:23 -0600

    ejemplo de una libreria paquetes documentos

---
+++ 16_Libreria/src/main/java/Controles/ArchivoL.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controles;
+
+/**
+ *
+ * @author Edgar
+ */
+
+//implementar la serializacion de los archivos
+class ArchivoL {
+    
+}
